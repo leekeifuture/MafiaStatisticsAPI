5d30f0dc910e363cca9d5fdb1f112ada68682286
Vladislav Brezovsky <opnoe09@gmail.com>
2021-09-06 12:08:35 +0300

    Initial commit

---
+++ src/main/java/com/mafia/statistics/MafiaStatisticsAPI/MafiaStatisticsApiApplication.java
@@ -0,0 +1,12 @@
+package com.mafia.statistics.MafiaStatisticsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaStatisticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafiaStatisticsApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mafia/statistics/MafiaStatisticsAPI/MafiaStatisticsApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mafia.statistics.MafiaStatisticsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaStatisticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
