c26f059a5be6c57ffff7ee42c73cdd358ab837b7
Vladislav Brezovsky <opnoe09@gmail.com>
2021-11-30 18:06:01 +0300

    Add migration for first user

---
+++ src/main/resources/db/migration/V2__Add_user.sql
@@ -0,0 +1,14 @@
+INSERT INTO player_dto (id, games_total, nickname, vk_id)
+VALUES (1, 0, 'Орк', 142419761);
+
+INSERT INTO role_dto (id, name)
+VALUES (2, 'USER');
+
+INSERT INTO role_dto (id, name)
+VALUES (3, 'ADMIN');
+
+INSERT INTO player_dto_roles (player_dto_id, roles_id)
+VALUES (1, 2);
+
+INSERT INTO player_dto_roles (player_dto_id, roles_id)
+VALUES (1, 3);
