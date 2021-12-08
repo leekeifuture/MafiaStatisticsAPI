cc495e1e2a73392384c2407c70514508e34ef6e1
Vladislav Brezovsky <opnoe09@gmail.com>
2021-12-08 20:09:46 +0300

    Edit number of counter to prevent +1 record

--- src/main/java/com/mafia/statistics/MafiaStatisticsAPI/service/impl/StatisticsService.java
+++ src/main/java/com/mafia/statistics/MafiaStatisticsAPI/service/impl/StatisticsService.java
@@ -297,7 +297,7 @@ public class StatisticsService implements IStatisticsService {
                 if ((statistics.getNicknameOfMafiaOne().equals(playerNickname) ||
                         statistics.getNicknameOfMafiaTwo().equals(playerNickname)) &&
                         statistics.getWins() > 1 &&
-                        counter.get() <= 10) {
+                        counter.get() <= 7) {
                     coupleStatisticsList.add(statistics);
                     counter.updateAndGet(v -> v + 1);
                 }
