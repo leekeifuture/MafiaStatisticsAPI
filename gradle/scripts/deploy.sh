bef7a67a658b264a7d9011465f56a9cb325657f6
Vladislav Brezovsky <opnoe09@gmail.com>
2022-10-16 21:19:46 +0300

    Fix deploy for MAC

--- gradle/scripts/deploy.sh
+++ gradle/scripts/deploy.sh
@@ -1,5 +1,5 @@
 aws ecr get-login-password --region eu-central-1 | \
   docker login --username AWS --password-stdin $ECR_URL
-docker build -t mafia-statistics .
+docker buildx build --platform=linux/amd64 -t mafia-statistics .
 docker tag mafia-statistics:latest $ECR_URL/mafia-statistics:latest
 docker push $ECR_URL/mafia-statistics:latest
