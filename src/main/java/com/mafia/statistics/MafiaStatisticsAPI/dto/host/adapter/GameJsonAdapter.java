b26d2c1eaee68044f067076cb5ca0ff2a284f7f0
Vladislav Brezovsky <opnoe09@gmail.com>
2023-03-27 23:36:21 +0300

    Add conditions to check if data is exists

--- src/main/java/com/mafia/statistics/MafiaStatisticsAPI/dto/host/adapter/GameJsonAdapter.java
+++ src/main/java/com/mafia/statistics/MafiaStatisticsAPI/dto/host/adapter/GameJsonAdapter.java
@@ -26,103 +26,146 @@ public class GameJsonAdapter implements JsonSerializer<Game> {
         String gameJson = gson.toJson(src);
         JsonObject game = JsonParser.parseString(gameJson).getAsJsonObject();
 
-        game.addProperty(
-                "hostId",
-                game.get("host").getAsJsonObject().get("id").getAsLong()
-        );
-        game.remove("host");
+        if (game.has("host") &&
+                game.get("host").getAsJsonObject().has("id")) {
+            game.addProperty(
+                    "hostId",
+                    game.get("host").getAsJsonObject().get("id").getAsLong()
+            );
+            game.remove("host");
+        }
 
-        game.addProperty(
-                "blackPlayerOneId",
-                game.get("blackPlayerOne").getAsJsonObject().get("id").getAsLong()
-        );
-        game.remove("blackPlayerOne");
+        if (game.has("blackPlayerOne") &&
+                game.get("blackPlayerOne").getAsJsonObject().has("id")) {
+            game.addProperty(
+                    "blackPlayerOneId",
+                    game.get("blackPlayerOne").getAsJsonObject().get("id").getAsLong()
+            );
+            game.remove("blackPlayerOne");
+        }
 
-        game.addProperty(
-                "blackPlayerTwoId",
-                game.get("blackPlayerTwo").getAsJsonObject().get("id").getAsLong()
-        );
-        game.remove("blackPlayerTwo");
+        if (game.has("blackPlayerTwo") &&
+                game.get("blackPlayerTwo").getAsJsonObject().has("id")) {
+            game.addProperty(
+                    "blackPlayerTwoId",
+                    game.get("blackPlayerTwo").getAsJsonObject().get("id").getAsLong()
+            );
+            game.remove("blackPlayerTwo");
+        }
 
-        game.addProperty(
-                "donPlayerId",
-                game.get("donPlayer").getAsJsonObject().get("id").getAsLong()
-        );
-        game.remove("donPlayer");
+        if (game.has("donPlayer") &&
+                game.get("donPlayer").getAsJsonObject().has("id")) {
+            game.addProperty(
+                    "donPlayerId",
+                    game.get("donPlayer").getAsJsonObject().get("id").getAsLong()
+            );
+            game.remove("donPlayer");
+        }
 
-        game.addProperty(
-                "sheriffPlayerId",
-                game.get("sheriffPlayer").getAsJsonObject().get("id").getAsLong()
-        );
-        game.remove("sheriffPlayer");
+        if (game.has("sheriffPlayer") &&
+                game.get("sheriffPlayer").getAsJsonObject().has("id")) {
+            game.addProperty(
+                    "sheriffPlayerId",
+                    game.get("sheriffPlayer").getAsJsonObject().get("id").getAsLong()
+            );
+            game.remove("sheriffPlayer");
+        }
 
-        game.addProperty(
-                "firstShootPlayerId",
-                game.get("firstShootPlayer").getAsJsonObject().get("id").getAsLong()
-        );
-        game.remove("firstShootPlayer");
-
-
-        JsonArray playersJsonArray = game.get("players").getAsJsonArray();
-
-        List<List<Number>> players = new ArrayList<>();
-
-        playersJsonArray.forEach(playerJsonElement -> {
-            JsonObject playerJsonObject = playerJsonElement.getAsJsonObject();
-
-            Long playerId = playerJsonObject
-                    .get("player").getAsJsonObject()
-                    .get("id").getAsLong();
-            Integer foulsCount = playerJsonObject.get("foulsCount").getAsInt();
-
-            List<Number> player = new ArrayList<>(List.of(playerId, foulsCount));
-            players.add(player);
-        });
-
-        game.remove("players");
-        game.add("players", gson.toJsonTree(players));
+        if (game.has("firstShootPlayer") &&
+                game.get("firstShootPlayer").getAsJsonObject().has("id")) {
+            game.addProperty(
+                    "firstShootPlayerId",
+                    game.get("firstShootPlayer").getAsJsonObject().get("id").getAsLong()
+            );
+            game.remove("firstShootPlayer");
+        }
 
 
-        JsonArray bestPlayersJsonArray = game.get("bestPlayers").getAsJsonArray();
+        if (game.has("players")) {
+            JsonArray playersJsonArray = game.get("players").getAsJsonArray();
 
-        List<List<Number>> bestPlayers = new ArrayList<>();
+            List<List<Number>> players = new ArrayList<>();
 
-        bestPlayersJsonArray.forEach(playerJsonElement -> {
-            JsonObject playerJsonObject = playerJsonElement.getAsJsonObject();
+            playersJsonArray.forEach(playerJsonElement -> {
+                JsonObject playerJsonObject = playerJsonElement.getAsJsonObject();
 
-            Long playerId = playerJsonObject
-                    .get("player").getAsJsonObject()
-                    .get("id").getAsLong();
-            Float additionalPoints = playerJsonObject.get("additionalPoints").getAsFloat();
+                if (!playerJsonObject.has("player") ||
+                        !playerJsonObject.get("player").getAsJsonObject().has("id") ||
+                        !playerJsonObject.has("foulsCount"))
+                    return;
 
-            List<Number> player = new ArrayList<>(List.of(playerId, additionalPoints));
-            bestPlayers.add(player);
-        });
-
-        game.remove("bestPlayers");
-        game.add("bestPlayers", gson.toJsonTree(bestPlayers));
-
-
-        JsonArray daysJsonArray = game.get("days").getAsJsonArray();
-
-        daysJsonArray.forEach(dayJsonElement -> {
-            JsonObject dayJsonObject = dayJsonElement.getAsJsonObject();
-            JsonArray votingMap = dayJsonObject.get("votingMap").getAsJsonArray();
-
-            votingMap.forEach(voting -> {
-                Long playerId = voting.getAsJsonObject()
+                Long playerId = playerJsonObject
                         .get("player").getAsJsonObject()
                         .get("id").getAsLong();
-                voting.getAsJsonObject().remove("player");
-                voting.getAsJsonObject().addProperty("playerId", playerId);
+                Integer foulsCount = playerJsonObject.get("foulsCount").getAsInt();
 
-                Long whoPutToVoteId = voting.getAsJsonObject()
-                        .get("whoPutToVote").getAsJsonObject()
-                        .get("id").getAsLong();
-                voting.getAsJsonObject().remove("whoPutToVote");
-                voting.getAsJsonObject().addProperty("whoPutToVoteId", whoPutToVoteId);
+                List<Number> player = new ArrayList<>(List.of(playerId, foulsCount));
+                players.add(player);
             });
-        });
+
+            game.remove("players");
+            game.add("players", gson.toJsonTree(players));
+        }
+
+        if (game.has("bestPlayers")) {
+            JsonArray bestPlayersJsonArray = game.get("bestPlayers").getAsJsonArray();
+
+            List<List<Number>> bestPlayers = new ArrayList<>();
+
+            bestPlayersJsonArray.forEach(playerJsonElement -> {
+                JsonObject playerJsonObject = playerJsonElement.getAsJsonObject();
+
+                if (!playerJsonObject.has("player") ||
+                        !playerJsonObject.get("player").getAsJsonObject().has("id") ||
+                        !playerJsonObject.has("additionalPoints"))
+                    return;
+
+                Long playerId = playerJsonObject
+                        .get("player").getAsJsonObject()
+                        .get("id").getAsLong();
+                Float additionalPoints = playerJsonObject.get("additionalPoints").getAsFloat();
+
+                List<Number> player = new ArrayList<>(List.of(playerId, additionalPoints));
+                bestPlayers.add(player);
+            });
+
+            game.remove("bestPlayers");
+            game.add("bestPlayers", gson.toJsonTree(bestPlayers));
+        }
+
+        if (game.has("days")) {
+            JsonArray daysJsonArray = game.get("days").getAsJsonArray();
+
+            daysJsonArray.forEach(dayJsonElement -> {
+                JsonObject dayJsonObject = dayJsonElement.getAsJsonObject();
+                if (!dayJsonObject.has("votingMap"))
+                    return;
+
+                JsonArray votingMap = dayJsonObject.get("votingMap").getAsJsonArray();
+
+                votingMap.forEach(votingMapJsonElement -> {
+                    JsonObject votingMapJsonObject = votingMapJsonElement.getAsJsonObject();
+
+                    if (!votingMapJsonObject.has("player") ||
+                            !votingMapJsonObject.get("player").getAsJsonObject().has("id") ||
+                            !votingMapJsonObject.has("whoPutToVote") ||
+                            !votingMapJsonObject.get("whoPutToVote").getAsJsonObject().has("id"))
+                        return;
+
+                    Long playerId = votingMapJsonObject
+                            .get("player").getAsJsonObject()
+                            .get("id").getAsLong();
+                    votingMapJsonObject.remove("player");
+                    votingMapJsonObject.addProperty("playerId", playerId);
+
+                    Long whoPutToVoteId = votingMapJsonObject
+                            .get("whoPutToVote").getAsJsonObject()
+                            .get("id").getAsLong();
+                    votingMapJsonObject.remove("whoPutToVote");
+                    votingMapJsonObject.addProperty("whoPutToVoteId", whoPutToVoteId);
+                });
+            });
+        }
 
         return gson.fromJson(game.toString(), JsonElement.class);
     }
